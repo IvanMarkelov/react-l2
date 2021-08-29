676fd87bffbf6efe6e0a52b70cc922f2b89b1108
И Марк <imark.work.ac@gmail.com>
2021-08-29 19:37:52 +0300

    Добавил второй инпут

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state.a = props.sideA;
+    this.state.h = props.height;
+    this.changeValueA = this.changeValueA.bind(this);
+    this.changeValueH = this.changeValueH.bind(this);
+  }
+
+  state = {
+    a: 12,
+    h: 10,
+  }
+
+  changeValueA(event) {
+    this.setState({a: event.target.value});
+   //!!! this.state.a = event.target.value;
+  }
+
+  changeValueH(event) {
+    this.setState((state) => {
+
+      return {
+        h: event.target.value
+      }
+    });
+  }
+
+  render() {
+    const answ = this.state.a * this.state.h / 2;
+    return (
+      <div className="App">
+        <input type="number" onChange={this.changeValueA} value={this.state.a}/>
+        <input type="number" onChange={this.changeValueH} value={this.state.h}/>
+        <h1>{answ}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App sideA="15" height="10"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
