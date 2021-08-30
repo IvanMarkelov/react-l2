ef8610a6c174a42abf6c5bef8d0c0302eb179e60
И Марк <imark.work.ac@gmail.com>
2021-08-30 21:38:48 +0300

    added counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0,
+    };
+
+    this.decrement = this.decrement.bind(this);
+    this.increment = this.increment.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  decrement() {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
+  reset() {
+    this.setState({
+      count: 0
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.decrement}>decrement</button>
+        <button onClick={this.increment}>increment</button>
+        <button onClick={this.reset}>reset</button>
+        <h1>{this.state.count}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
