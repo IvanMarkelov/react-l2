0252648464a7307939b5332d1fa301f03991146c
И Марк <imark.work.ac@gmail.com>
2021-09-02 19:11:21 +0300

    solved task

--- src/App.js
+++ src/App.js
@@ -1,66 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
 
-function Car(props) {
-  const classes = ["card"];
-
-  if (props.car.marked) {
-    classes.push("marked");
-  }
-
-  return (
-    <div
-      style={{
-        border: "1px black solid",
-        borderRadius: "15px",
-        maxWidth: "25%",
-        padding: "5px 5px",
-        margin: "5px 5px",
-      }}
-      className={classes.join(' ')}
-      onClick={props.onMark}
-    >
-      <img style={{maxWidth: "80%"}} src={props.car.imageSource} alt={props.car.name}/>
-      <h3>Наименование: {props.car.name}</h3>
-      <p>Стоимость проката/час: {props.car.price}</p>
-      <p>Уровень заряда: {props.car.chargeLevel}%</p>
-    </div>
-  );
-}
-
 class App extends Component {
   state = {
-    cars: [
-      { marked: false, imageSource: "./trollo.jpg", chargeLevel: 75, name: "Trollo", price: 200 },
-      { marked: false, imageSource: "./mgp.jpg", chargeLevel: 25, name: "MGP", price: 150 },
-      { marked: false, imageSource: "./swifty.jpg", chargeLevel: 90, name: "Swifty", price: 250 },
-      { marked: false, imageSource: "./foxpro.jpg", chargeLevel: 100, name: "FOX Pro", price: 250 },
-      { marked: false, imageSource: "./crops.png", chargeLevel: 80, name: "Crops", price: 230 },
-      { marked: false, imageSource: "./melon.jpg", chargeLevel: 40, name: "Melon", price: 200 },
-    ],
+    languages: ["html", "css", "js", "php"],
   };
 
-  handleMarked(name) {
-    const cars = this.state.cars.concat();
-
-    const car = cars.find((c) => c.name === name);
-    car.marked = !car.marked;
-
-    this.setState({
-      cars: cars,
+  populateList() {
+    return this.state.languages.map((language) => {
+      return <li>{language}</li>;
     });
   }
 
   render() {
-    return this.state.cars.map((car) => {
-      return (
-        <Car 
-          car={car}
-          key={car.name + Math.random()}
-          onMark={this.handleMarked.bind(this, car.name)}
-        />
-      );
-    });
+    return <ul>
+      {
+        this.populateList()
+      }
+    </ul>
+
   }
 }
 
