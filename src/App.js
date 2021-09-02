e094fafb683273d633d6c8908f56fdbcd9ff6b7f
И Марк <imark.work.ac@gmail.com>
2021-09-02 08:56:24 +0300

    task resolved

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
+import { show } from "@blueprintjs/core/lib/esm/components/context-menu/contextMenu";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    show: true,
+    name: "Ivan",
+  };
+
+  changeState = () => {
+    const show = this.state.show;
+    this.setState({
+      show: !show,
+    });
+  };
+
+  render() {
+    const changeStateTo = !this.state.show;
+    if (this.state.show) {
+      return (
+        <div className="App">
+          <p>Привет, {this.state.name}!</p>
+          <button onClick={this.changeState}>Change State to {changeStateTo.toString()}</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <p>Пока, {this.state.name}</p>
+          <button onClick={this.changeState}>Change State to {changeStateTo.toString()}</button>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
